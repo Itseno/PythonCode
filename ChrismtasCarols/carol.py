2446d65fc2617ebe96f5594b96b515e33e98da50
Ian Crossey <crossei@stlprioryschool.org>
2019-01-06 18:00:08 -0600

    Carols

--- ChrismtasCarols/carol.py
+++ ChrismtasCarols/carol.py
@@ -1,9 +1,5 @@
 import operator
 
-top_count = int(input("How many do you want to display?"))
-while top_count < 1:
-    top_count = int(input("How many do you want to display?"))
-
 alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM "
 
 def clean(input):
@@ -13,19 +9,41 @@ def clean(input):
             clean += character
     return clean.lower()
 
-counts = {}
-input = clean(open('13-Alice.txt').read())
+countCarols = {}
+countCommon = {}
 
-for word in input.split():
-    if word not in counts:
-        counts[word] = 0
-    counts[word] += 1
+carols = input("Where is the carols file?")
+common = input("Where is the common file?")
 
-counts = sorted(counts.items(), key=operator.itemgetter(1), reverse=True)
+top_count = int(input("How many do you want to display?"))
+while top_count < 1:
+    top_count = int(input("How many do you want to display?"))
 
-top = counts[:top_count]
+inputCarols = clean(open(carols).read())
+inputCommon = clean(open(common).read())
+
+for word in inputCarols.split():
+    if word not in countCarols:
+        countCarols[word] = 0
+    countCarols[word] += 1
+
+for word in inputCommon.split():
+    if word not in countCommon:
+        countCommon[word] = 0
+    countCommon[word] += 1
+
+countCarols = sorted(countCarols.items(), key=operator.itemgetter(1), reverse=True)
+countCommon = sorted(countCommon.items(), key=operator.itemgetter(1), reverse=True)
+
+topCarols = countCarols[:top_count]
+topCommon = countCommon[:top_count]
 
 count = 1
-for item in top:
+for item in topCarols:
     print("#" + str(count) + "\t\t" + item[0] + "\t\t" + str(item[1]))
     count += 1
+
+twocount = 1
+for item in topCommon:
+    print("#" + str(count) + "\t\t" + item[0] + "\t\t" + str(item[1]))
+    twocount += 1
