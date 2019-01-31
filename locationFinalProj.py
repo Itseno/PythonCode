2b1013d07fe81b114c61a554369c9271dcbb0c97
Ian Crossey <crossei@stlprioryschool.org>
2019-01-30 18:47:21 -0600

    PRoj

--- locationFinalProj.py
+++ locationFinalProj.py
@@ -1,7 +1,8 @@
 game = True
 game_time = True
+import time
 location = "Town_square"
-name = "John"
+name = "Maeth"
 
 while game == True:
     if location == "Town_square":
@@ -9,21 +10,33 @@ while game == True:
             print("A magical knight battles a warlord. The crowd arround you cheers. The sun beat down relentlessly.")
             print("*Kaboom*")
             print("--------------")
+            time.sleep(0.5)
+            print("--------------")
+            time.sleep(1)
             print("Find me " + name + " for you are of pure heart.")
+            print("--------------")
+            time.sleep(2)
             print("--------------")
             game_time = False
         if game == True:
-            print(name + ", are you okay?")
             print("")
             print("Criaf, your friend from hometown stands above you.")
-            follow_him = input(name + ", are you okay? Let's head back to the inn.")
-            if follow_him == "ok":
+            input(name + ", are you okay?")
+            follow_him = input("That explosion was crazy! I think we should head to the inn before something bad happens. What do you think? ")
+            if follow_him == "ok" or "yes" or "follow":
                 print("You follow him.")
                 location = "inn"
+                criaf_location = "inn"
             elif follow_him == "look":
-                print("The town square is in chaos, people are running from the arena. The knight, and warlord are nowhere to be found.")
-                print("John heads back to the inn to the east.")
-            elif follow_him == "No":
-                print("What? Please John.")
+                print("The town square is in chaos, people are running from the arena. The knight, and warlord are nowhere to be found. ")
+                print("Criaf heads back to the inn to the east")
+                criaf_location = "inn"
+            elif follow_him == "no":
+                print("Ok, maeth, I will see you there later.")
+                print("Criaf heads back to the inn to the east.")
+                criaf_location = "inn"
+            elif
     if location == "inn":
-        print("You approach an inn.")
+        print("You approach a large inn.")
+        char_inn = input("")
+        if char_do == "n":
